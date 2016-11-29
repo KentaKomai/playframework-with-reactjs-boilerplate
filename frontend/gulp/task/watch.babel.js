c565a6e071903b7a7aa33acc87e33fa1345e9106
kkomai <komaikenta@team-lab.com>
2016-11-30 00:20:33 +0900

    delete

--- frontend/gulp/task/watch.babel.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp'
-
-gulp.task('watch', ['build'], () => {
-  gulp.watch(['assets/sources/**/*.js'], ['lint', 'webpack'])
-  gulp.watch(['assets/sources/**/*.jsx'], ['lint', 'webpack'])
-  gulp.watch(['assets/sources/**/*.styl'], ['stylus'])
-  gulp.watch(['assets/sources/media/*'], ['media'])
-  //gulp.watch(['assets/sources/sharder/*'], ['sharder'])
-})
